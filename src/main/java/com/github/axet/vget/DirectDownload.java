eec71b69c1fa884c90db2120317f222e20b8f20e
Allwin Savari Raj <allwin.amazon@gmail.com>
2014-06-24 23:07:23 +0530

    Added Scanner for the file

--- src/main/java/com/github/axet/vget/DirectDownload.java
+++ src/main/java/com/github/axet/vget/DirectDownload.java
@@ -3,17 +3,26 @@ package com.github.axet.vget;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Scanner;
 
 public class DirectDownload {
 
     public static void main(String[] args) {
         try {
             // ex: http://www.youtube.com/watch?v=Nj6PFaDmp6c
-            String url = args[0];
+        	String url = "", path = "";
+            //String url = args[0];
             // ex: "/Users/axet/Downloads"
-            String path = args[1];
+            //String path = args[1];
+            
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the URL : ");
+            url = in.nextLine();
+            System.out.println("Enter the Path (to save) :");
+            path = in.nextLine();
             VGet v = new VGet(new URL(url), new File(path));
             v.download();
+            System.out.println("Done");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
